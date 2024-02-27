a7590f7cd312be0bd2bbc9d64ee73dffbab61e6c
henriquejunqueira <henriquejunqueira15@gmail.com>
2024-02-27 10:10:48 -0300

    inserindo eslint no projeto

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    semi: true, // adiciona ; ao final da linha quando necessário
+    trailingComma: 'all', // adiciona , ao final da linha quando necessário
+    singleQuote: true, // coloca sempre '' ao inves de ""
+    printWidth: 100, // quebra a linha quando chegar no limite especificado de caracteres
+    tabWidth: 2, // configura o número de tabs na indentação
+};
